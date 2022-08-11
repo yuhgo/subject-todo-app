5286992c42e5a7ee6561c65a4cdc0161f4ae6c36
ゆうご <yuhgo090764@gmail.com>
2022-08-12 02:22:44 +0900

    🔧 MantineUIとTailwindCSSの設定を追加

---
+++ src/lib/mantine/index.ts
@@ -0,0 +1,3 @@
+export { Button } from "./Button"
+export { useMediaQuery } from "./useMediaQuery"
+export { useViewportSize } from "./useViewportSize"
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    // Mantine UI側のサイズに合わせる
+    screen: {
+      xs: "576px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1400px",
+    },
   },
   plugins: [],
 }
